a61f3ae64d3582579bedafa100227460da33d7f9
b1aiirrr <tonyblaiirr@gmail.com>
2025-12-07 12:44:55 +0300

    41st

--- snack-have-hun/vite.config.js
+++ snack-have-hun/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'Snack Have Hun',
+        short_name: 'SnackHaveHun',
+        description: 'Kenyan Street Food, Elevated.',
+        theme_color: '#ea580c',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/food/hero_fries.jpg', // Placeholder icon using your existing image
+            sizes: '512x512',
+            type: 'image/jpeg'
+          }
+        ]
+      }
+    })
+  ],
 })
